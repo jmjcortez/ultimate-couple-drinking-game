ec35524a81431b516faac6450a1b72cf3e2ea612
Jared <jaredmartincortez@Jareds-New-MacBook-Pro.local>
2022-04-20 18:31:13 +1000

    init commit

--- README.md
+++ README.md
@@ -1,70 +1,14 @@
-# Getting Started with Create React App
+<b>Ultimate Couple Drinking Game</b>
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Rules: 
+- Both couples take turns in answering the questions
+- The format of questions and rules goes like this
+  - First 10: Usual Questions. Take a shot if you don't want to answer the question
+  - #11 and #12: How well do you know your partner question? 
+  - #13 to #18: Most likely (cooperation)
+    -  both of you will answer the question
+    - if you have different answers, both of you has to take a shot
+    - if you have similar answers, both of you are safe
+  - #19 and #20: Dare. If you don't wanna do it, you take a shot
 
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+<b>Have Fun</b>
--- src/App.js
+++ src/App.js
@@ -1,23 +1,163 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
+import {
+  questions as questionsDb,
+  dares as daresDb,
+  howWell as howWellDb,
+  mostLikely as mostLikelyDb,
+} from "./data";
+
 function App() {
+
+  const [question, setQuestion] = useState('')
+  const [questions, setQuestions] = useState(questionsDb)
+  const [dares, setDares] = useState(daresDb)
+  const [howWell, setHowWell] = useState(howWellDb)
+  const [mostLikely, setMostLikely] = useState(mostLikelyDb)
+  const [firstPlayerScore, setFirstPlayerScore] = useState(0)
+  const [secondPlayerScore, setSecondPlayerScore] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [status, setStatus] = useState('question')
+  const [counter, setCounter] = useState(-1)
+
+  const generateQuestion = async () => {
+
+    let index;
+
+    if (counter < 10) {
+      setStatus('Question')
+
+      index = questions[Math.floor(Math.random() * questions.length)];
+      setQuestion(index);
+      setQuestions(questions.filter((q) => ( q !== index )));
+      // await setQuestions(questions.splice(index, 1));
+      console.log(questions);
+    }
+    // else if (counter >= 10 && counter < 16) {
+    else if (counter >= 12 && counter < 18) {
+      setStatus('Most Likely')
+      console.log(mostLikely)
+      index = mostLikely[Math.floor(Math.random() * mostLikely.length)];
+      setQuestion(index);
+      setMostLikely(mostLikely.filter((q) => ( q !== index )));
+      
+    }
+    else if (counter >= 10 && counter < 12) {
+    // else if (counter >= 16 && counter < 18) {
+      setStatus('How Well')
+
+      index = howWell[Math.floor(Math.random() * howWell.length)];
+      setQuestion(index);
+      setHowWell(howWell.filter((q) => ( q !== index )));
+      
+    }
+    else {
+      setStatus('dare')
+
+      index = dares[Math.floor(Math.random() * dares.length)];
+      setQuestion(index);
+      setDares(dares.filter((q) => ( q !== index )));
+      
+    }
+
+    if (counter < 19) {
+      setCounter(counter + 1)
+    }
+
+    if (counter === 19) {
+      setCounter(0)
+    }
+
+  }
+
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+
+  const toggleLoading = async () => {
+    setLoading(true);
+    // await delay(1500)
+    await delay(500)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    return () => {
+      // return setQuestion( questionsDb[Math.floor(Math.random() * questionsDb.length)] );
+      return generateQuestion();
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='sides left-side'>
+        <h1 className='shot-counter'>Shot counter</h1>
+
+        <div className='players'>
+          <div className='player-name-container left-player'>
+            <h1 className='score'>
+              {firstPlayerScore}
+            </h1>
+            <div className='name'>Jared</div>
+            <button
+              className='plusButton'
+              onClick={() => {
+                setFirstPlayerScore(firstPlayerScore + 1)
+              }}
+            >+</button>
+          </div>
+          <div className='player-name-container right-player'>
+            <h1 className='score'>
+              {secondPlayerScore}
+            </h1>
+            <div className='name'>Olena</div>
+            <button
+              className='plusButton'
+              onClick={() => {
+                setSecondPlayerScore(secondPlayerScore + 1)
+              }}
+            >+</button>
+          </div>
+        </div>
+
+        <button className='generate-button' onClick={() => {
+          toggleLoading(true);
+          generateQuestion();
+        }}>
+          Generate Question
+        </button>
+      </div>
+      <div className='sides right-side'>
+        {
+          loading ? (
+            <div className='loader'>
+              <div className='outer'></div>
+              <div className='middle'></div>
+              <div className='inner'></div>
+              <div className='dot'></div>
+            </div>
+          ) :
+            (
+              <div className='card'>
+                <div className='face1'>
+                  <div className='img-box'>
+                    <h1>{status}</h1>
+                  </div>
+                </div>
+                <div className='face2'>
+                  <div className='content'>
+                    <p className=''>
+                      {question}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )
+        }
+
+
+      </div>
+
     </div>
   );
 }
